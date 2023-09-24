b60af3c2c1ae270a53cd45aa1106fa3ca25556f3
mangoleaf69 <mangoleaf96d343@gmail.com>
2023-09-24 08:55:20 -0700

    Pub:0 🍺 ...

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-git add *
-git commit im "Pub:0 🍺 ... $1"
-git push
-
+git add * ;
+git commit -m "Pub:0 🍺 ... $1" ;
+git push ;
+echo
 echo "Crack open a cold one while cloudflare deploys the page and if your on master view it at https://mangoleaf.world"
